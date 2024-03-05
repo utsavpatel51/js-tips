24ac64b0e4977e3bb6490cb624d2fa274ace0484
Utsav Patel <utsavpatel51@gmail.com>
2024-03-06 01:17:05 +0530

    Adding plop generate blog template

--- src/components/common/mdx/code-block.tsx
+++ src/components/common/mdx/code-block.tsx
@@ -1,9 +1,60 @@
+'use client';
+import { CheckCheckIcon, CopyIcon } from 'lucide-react';
 import React from 'react';
 
-export default function CodeBlock({ children }: { children: React.ReactNode }) {
+interface Props {
+  children: React.ReactNode;
+  lang: string;
+}
+
+export default function CodeBlock({ children, lang }: Props) {
+  const [isCopied, setIsCopied] = React.useState(false);
+
+  const copyToClipboard = () => {
+    const dataBlock = document.getElementById('code-block');
+
+    const textToCopy = (
+      dataBlock?.childNodes?.[0].childNodes[0] as unknown as {
+        outerText: string;
+      }
+    ).outerText;
+
+    navigator.clipboard.writeText(textToCopy);
+    setIsCopied(true);
+  };
+
+  const handleAway = () => {
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
   return (
-    <pre>
-      <code>{children}</code>
-    </pre>
+    <div className='group border'>
+      <div className='py-1 px-2 flex flex-row justify-end'>
+        <span
+          className='text-sm cursor-pointer active:scale-110'
+          onClick={copyToClipboard}
+          onMouseLeave={handleAway}
+          role='button'
+        >
+          {isCopied ? (
+            <p className='flex flex-row items-center gap-x-1 text-green-600'>
+              COPIED <CheckCheckIcon className='h-4 w-4' />
+            </p>
+          ) : (
+            <p className='flex flex-row items-center gap-x-1'>
+              COPY <CopyIcon className='h-3 w-3' />
+            </p>
+          )}
+        </span>
+      </div>
+      <div
+        className='pb-4 px-10 text-start inline-block w-full'
+        id='code-block'
+      >
+        {children}
+      </div>
+    </div>
   );
 }
